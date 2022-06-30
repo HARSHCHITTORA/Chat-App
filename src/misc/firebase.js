3a8fecd677331da4b78b0910150aa3d4ce85865d
Chittox <harshchittora2001@gmail.com>
2022-06-30 10:47:33 +0530

    create and config firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config= {
+    apiKey: "AIzaSyBtCheb8FyI3dhITEup6fifj82VnBJlebk",
+    authDomain: "chittox-chat-app.firebaseapp.com",
+    projectId: "chittox-chat-app",
+    storageBucket: "chittox-chat-app.appspot.com",
+    messagingSenderId: "767434574817",
+    appId: "1:767434574817:web:42215a182f4af1c57520a0"
+  };
+  
+  const app=firebase.initializeApp(config) 
