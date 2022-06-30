a3a2bd8221e7c6a1d57103eda314c4de516c726e
Chittox <harshchittora2001@gmail.com>
2022-06-30 11:26:40 +0530

    Creating pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@
 import React from "react";
 import './styles/main.scss'
 import 'rsuite/dist/rsuite.min.css'
+import { Switch } from "react-router";
+import Signin from './pages/Signin'
+import PrivateRoute from './components/PrivateRoutes'
+import PublicRoute from './components/PublicRoute'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>hello</div>
+   <Switch>
+    <PublicRoute path='/signin'>
+       <Signin/>
+    </PublicRoute>
+
+    <PrivateRoute path='/'>
+      <Home/>
+    </PrivateRoute>
+   </Switch>
   );
 }
 
