34edee9e867098d00862d5447c0002a8b5be0734
Chittox <harshchittora2001@gmail.com>
2022-07-01 13:58:31 +0530

    Creating Chat Rooms List - Show rooms and use them as links (Part 3)

--- src/components/rooms/ChatRoomList.jsx
+++ src/components/rooms/ChatRoomList.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Nav } from 'rsuite';
 import RoomItem from './RoomItems';
+import { Nav, Loader } from 'rsuite';
+import { useRooms } from '../../context/rooms.context';
+import { Link, useLocation } from 'react-router-dom';
 
-const ChatRoomList = ({ aboveElHeight }) => {
+
+
+    const ChatRoomList = ({ aboveElHeight }) => {
+        const rooms = useRooms();
+        const location = useLocation();
+    
   return (
     <Nav
       appearance="subtle"
@@ -12,12 +19,27 @@ const ChatRoomList = ({ aboveElHeight }) => {
       style={{
         height: `calc(100% - ${aboveElHeight}px)`,
       }}
+    activeKey={location.pathname}
+    
     >
-      <Nav.Item>
-        <RoomItem />
+      {!rooms && (
+        <Loader center vertical content="Loading" speed="slow" size="md" />
+      )}
+      {rooms &&
+        rooms.length > 0 &&
+        rooms.map(room => (
+      <Nav.Item
+      componentClass={Link}
+            to={`/chat/${room.id}`}
+            key={room.id}
+            eventKey={`/chat/${room.id}`}
+            >
+        <RoomItem room={room} />
       </Nav.Item>
+    ))}
     </Nav>
   );
 };
+
 
 export default ChatRoomList;
