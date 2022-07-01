917a6276c8cb3914339465695a025c60eeebf9e5
Chittox <harshchittora2001@gmail.com>
2022-07-01 10:51:07 +0530

    Creating Chat Rooms Lists - Part 1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
+import {Divider} from 'rsuite'
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
+const topSidebarRef=useRef()
+const [height,setHeight]=useState(0)
+
+useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal/>
+        <Divider>Join</Divider>
       </div>
-     
+     <ChatRoomList  aboveElHeight={height}/>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../components/Sidebar';
 const Home = () => {
   return (
     <Grid fluid className="h-100">
-      <Row>
+      <Row className='h-100'>
         <Col xs={24} md={8}>
           <Sidebar />
         </Col>
